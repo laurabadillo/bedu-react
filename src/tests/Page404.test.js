aa7039e38bd77a5d106ec2329e9264576576eeef
Laura Badillo <badillo_laura@yahoo.com>
2019-08-24 13:54:09 -0500

    :white_check_mark: Update test

--- src/tests/Page404.test.js
+++ src/tests/Page404.test.js
@@ -7,4 +7,11 @@ describe('Page404 component', () => {
   it ('should do the correct operation', () => {
     expect(1 + 1).toBe(2);
   });
+  it ('should render Page404 component', () => {
+    const wrapper = shallow(<Page404 />);
+    const h3 = wrapper.find('h3'); 
+    const result = h3.text(); 
+
+    expect(result).toBe('Page not found');
+  });
 });
